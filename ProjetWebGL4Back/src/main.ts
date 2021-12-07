58d49ba5408f2c8517c3c38a5eb503274afa2862
Ghassen Zakraoui <Ghassen.zakraoui@expensya.com>
2021-12-07 12:03:21 +0200

    add the auth module

--- ProjetWebGL4Back/src/main.ts
+++ ProjetWebGL4Back/src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const corsOptions = {
+    origin: ['http://localhost:4200'],
+  };
+  app.enableCors(corsOptions);
+
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
