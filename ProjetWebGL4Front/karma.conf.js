4c6f8df6a0b140f9b40918bbedfd34dcbd754fee
Ramez Ben Aribia <ramezbenaribia@gmail.com>
2021-12-14 18:28:43 +0200

    lower angular version, add cool dependencies and styles + assets

--- ProjetWebGL4Front/karma.conf.js
+++ ProjetWebGL4Front/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/projetgl4'),
+      dir: require('path').join(__dirname, './coverage/bascalni-front'),
       subdir: '.',
       reporters: [
         { type: 'html' },
