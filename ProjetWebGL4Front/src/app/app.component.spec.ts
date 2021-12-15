63c2b78b5af938a34c004afe82fec9a68334305f
Souha BH <souha.benhassine@insat.u-carthage.tn>
2021-12-15 11:23:01 +0200

    About us component, assets and clean up

--- ProjetWebGL4Front/src/app/app.component.spec.ts
+++ ProjetWebGL4Front/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'projetgl4'`, () => {
+  it(`should have as title 'bascalni-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('projetgl4');
+    expect(app.title).toEqual('bascalni-front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('projetgl4 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('bascalni-front app is running!');
   });
 });
--- ProjetWebGL4Front/src/app/app.module.ts
+++ ProjetWebGL4Front/src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { EventsService } from 'src/app/upcoming-events/events.service';
+import { TokenInterceptor } from './interceptors/login.interceptor';
+import { LoginGuard } from './guards/login.guard';
+import { LogoutGuard } from './guards/logout.guard';
+import { AboutUsComponent } from './about-us/about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AboutUsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    EventsService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true,
+    },
+    LoginGuard,
+    LogoutGuard,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
